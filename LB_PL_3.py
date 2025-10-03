c9e61fadc8b16d2bf0dd6b3bca1c82a6196e46df
Egorov Ars <ArsenMaxEgorov@mail.ru>
2025-10-03 20:24:10 +0300

    Add to LB_PL_3 exersice 3

--- LB_PL_3.py
+++ LB_PL_3.py
@@ -5,3 +5,8 @@ print([i**2 for i in range(1, 11)])
 # Задание 2
 
 print([i for i in range(2, 21, 2)])
+
+# Задание 3
+
+words = ["python", "Java", "C++", "Rust", "go"]
+print([i for i in words if i[0].isupper() and len(i)>3])
