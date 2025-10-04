30b1e7b79a7284e676005cce0f6ff71ac24823e2
Egorov Ars <ArsenMaxEgorov@mail.ru>
2025-10-04 06:09:11 +0300

    Add to LB_PL_3 exersice 5

--- LB_PL_3.py
+++ LB_PL_3.py
@@ -1,18 +1,18 @@
 # Задание 1
-
+print("Задание 1")
 print([i**2 for i in range(1, 11)])
 
 # Задание 2
-
+print("Задание 2")
 print([i for i in range(2, 21, 2)])
 
 # Задание 3
-
+print("Задание 3")
 words = ["python", "Java", "C++", "Rust", "go"]
 print([i.upper() for i in words if len(i)>3])
 
 # Задание 4
-
+print("Задание 4")
 class Countdown:
     def __init__(self, n):
         self.n = n
@@ -30,3 +30,15 @@ class Countdown:
 
 for i in Countdown(5):
     print(i)
+    
+# Задание 5
+print("Задание 5")
+def fibonacci(n):
+    a, b = 0, 1
+    k = 0
+    while k < n:
+        yield a
+        a = b
+        b = a + b
+        k += 1
+for i in fibonacci(5): print(i)
